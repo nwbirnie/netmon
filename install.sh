3214c258e8a6c2c27576a51ddfff61c5ec76ae8a
Nick Birnie <nwbirnie@github.com>
2023-04-23 18:46:01 +0100

    Rebuild prometheus on install

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 set -euox pipefail
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "${SCRIPT_DIR}"
+docker/prometheus/install.sh
 cp -r usr/* /usr
 cp -r etc/* /etc
 mkdir -p /var/prometheus
