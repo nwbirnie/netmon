a4c3a4947babf036cb096ec8607f0f58d9a5b7e5
Nick Birnie <nwbirnie@github.com>
2023-04-23 18:14:47 +0100

    Change prometheus image and specify config file

--- usr/local/bin/prometheus.sh
+++ usr/local/bin/prometheus.sh
@@ -4,4 +4,4 @@ docker run \
   -p 9090:9090 \
   -v /usr/local/share/netmon/prometheus.yml:/etc/prometheus/prometheus.yml \
   -v /var/prometheus:/prometheus \
-  prom/prometheus --storage.tsdb.retention.size=1GB
+  nick/prometheus:latest --storage.tsdb.retention.size=1GB --config.file=/etc/prometheus/prometheus.yml
