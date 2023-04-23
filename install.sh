253d9e99ab1c640a8ea620a44ee8cbfa2b8b3a53
Nick Birnie <nwbirnie@github.com>
2023-04-23 20:27:59 +0100

    Configure grafana user+password on install

--- install.sh
+++ install.sh
@@ -5,10 +5,17 @@ cd "${SCRIPT_DIR}"
 docker/prometheus/install.sh
 docker/blackbox_exporter/install.sh
 docker/fastcom/install.sh
-sudo cp -r usr/* /usr
-sudo cp -r etc/* /etc
-sudo mkdir -p /var/prometheus
-sudo chmod a+w /var/prometheus
-sudo systemctl enable blackbox.service
-sudo systemctl enable fastcom.service
-sudo systemctl enable prometheus.service
+
+read -p 'Enter grafana service account credential: ' grafana_credential
+export grafana_credential
+cat <<EOF | sudo bash
+cp -r usr/* /usr
+cp -r etc/* /etc
+mkdir -p /var/prometheus/secret
+echo "${grafana_credential}" > /var/prometheus/secret/grafana.credential
+chmod a+w /var/prometheus
+systemctl enable blackbox.service
+systemctl enable fastcom.service
+systemctl enable prometheus.service
+EOF
+
--- usr/local/bin/prometheus.sh
+++ usr/local/bin/prometheus.sh
@@ -4,4 +4,5 @@ docker run \
   -p 9090:9090 \
   -v /usr/local/share/netmon/prometheus.yml:/etc/prometheus/prometheus.yml \
   -v /var/prometheus:/prometheus \
+  -v /var/prometheus/secret/grafana.credential:/grafana.credential \
   nick/prometheus:latest --storage.tsdb.retention.size=1GB --config.file=/etc/prometheus/prometheus.yml
