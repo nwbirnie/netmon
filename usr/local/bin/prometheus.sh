d4ea0c585ba79c03e3e0c7704be45590cccc3c03
Nick Birnie <nwbirnie@github.com>
2023-04-23 17:57:44 +0100

    Move prometheus data to /var

--- usr/local/bin/prometheus.sh
+++ usr/local/bin/prometheus.sh
@@ -3,5 +3,5 @@ docker run \
   --rm \
   -p 9090:9090 
   -v /usr/local/share/netmon/prometheus.yml:/etc/prometheus/prometheus.yml \
-  -v prometheus_data:/prometheus \
+  -v /var/prometheus:/prometheus \
   prom/prometheus --storage.tsdb.retention.size=1GB
