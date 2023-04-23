ed5ed16fcacd1ac44b148b277a21f5269c12e5bd
Nick Birnie <nwbirnie@github.com>
2023-04-23 18:05:01 +0100

    Add install script

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -euox pipefail
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd "${SCRIPT_DIR}"
+cp -r usr/* /usr
+cp -r etc/* /etc
+mkdir -p /var/prometheus
+chmod a+w /var/prometheus
