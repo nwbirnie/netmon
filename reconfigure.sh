0d2569d48ce9545787d9cfd37d14acb85ef688a5
Nick Birnie <none@none.com>
2023-06-17 23:14:29 +0100

    Fix some stuff

--- reconfigure.sh
+++ reconfigure.sh
@@ -1,25 +1,30 @@
 #/bin/bash
 set -euo pipefail
 
-read -p 'Enter grafana user name: ' grafana_user
-read -p 'Enter grafana access policy token: ' grafana_credential
-read -p 'Enter grafana URL: ' grafana_url
+[[ ! -z "${grafana_user}" ]] || read -p 'Enter grafana user name: ' grafana_user
+[[ ! -z "${grafana_credential}" ]] || read -p 'Enter grafana access policy token: ' grafana_credential
+[[ ! -z "${grafana_url}" ]] || read -p 'Enter grafana URL: ' grafana_url
 
 set -x
 
 export grafana_credential grafana_url grafana_user
 
-at <<EOF | sudo bash
+cat <<EOF | sudo bash
 set -euox pipefail
 cp -r usr/* /usr
 cp -r etc/* /etc
 mkdir -p /var/prometheus/secret
 mkdir -p /var/prometheus/data
+mkdir -p /var/log/netmon
 chmod a+w -R /var/prometheus/data
+chmod a+w -R /var/log/netmon
+
+useradd -s /usr/sbin/nologin netmon || true
+usermod -G docker netmon || true
+
 echo "${grafana_credential}" > /var/prometheus/secret/grafana.credential
 sed "s/INSERT_USER/${grafana_user}/" -i /usr/local/share/netmon/prometheus.yml
 sed "s,INSERT_URL,${grafana_url}," -i /usr/local/share/netmon/prometheus.yml
-chmod a+w /var/prometheus
 systemctl enable blackbox.service
 systemctl enable fastcom.service
 systemctl enable prometheus.service
