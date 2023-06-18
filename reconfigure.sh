f068e1f70b225381145312eecf8d87d120a910ff
Nick Birnie <none@none.com>
2023-06-18 17:52:18 +0100

    Fix path to prom conf

--- reconfigure.sh
+++ reconfigure.sh
@@ -1,9 +1,9 @@
 #/bin/bash
 set -euo pipefail
 
-[[ ! -z "${grafana_user}" ]] || read -p 'Enter grafana user name: ' grafana_user
-[[ ! -z "${grafana_credential}" ]] || read -p 'Enter grafana access policy token: ' grafana_credential
-[[ ! -z "${grafana_url}" ]] || read -p 'Enter grafana URL: ' grafana_url
+[[ -z "${grafana_user}" ]] && read -p 'Enter grafana user name: ' grafana_user
+[[ -z "${grafana_credential}" ]] && read -p 'Enter grafana access policy token: ' grafana_credential
+[[ -z "${grafana_url}" ]] && read -p 'Enter grafana URL: ' grafana_url
 
 set -x
 
@@ -27,11 +27,15 @@ chmod 755 -R /var/prometheus
 useradd -s /usr/sbin/nologin netmon || true
 usermod -G docker netmon || true
 
-echo "${grafana_credential}" > /var/prometheus/secret/grafana.credential
-sed "s/INSERT_USER/${grafana_user}/" -i /usr/local/share/netmon/prometheus.yml
-sed "s,INSERT_URL,${grafana_url}," -i /usr/local/share/netmon/prometheus.yml
+touch /var/prometheus/secret/grafana.credential
+chown nobody:nogroup /var/prometheus/secret/grafana.credential
 chmod 400 /var/prometheus/secret/grafana.credential
+
+echo "${grafana_credential}" > /var/prometheus/secret/grafana.credential
+sed "s/INSERT_USER/${grafana_user}/" -i /etc/netmon/prometheus.yml
+sed "s,INSERT_URL,${grafana_url}," -i /etc/netmon/prometheus.yml
 
 systemctl enable blackbox.service fastcom.service prometheus.service
 systemctl restart blackbox.service fastcom.service prometheus.service
+
 EOF
